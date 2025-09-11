cc803dc8e288e0d4463b4eab16b8a0c774514fdf
Mehdi Selmi <selmimehdi970@gmail.com>
2025-09-11 18:34:50 +0100

    Add build files for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+<<<<<<< HEAD
       base: "/FoodEat/","git@github.com:mehdiselmi/FoodEat.git"
 
+=======
+    base: "/FoodEat/","FoodEat":"git@github.com:mehdiselmi/FoodEat.git"
+>>>>>>> e32adcd (Add build files for GitHub Pages)
 })
